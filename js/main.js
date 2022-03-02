bef78343f1e31ff410e3fcd92ef426ec2df1e633
Alex-Uva89 <a.uva1988@gmail.com>
2022-03-02 17:04:27 +0100

    complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 
 let areaContainBoxes = document.getElementById('box_game');
+const checkBox = document.querySelectorAll('.box');
+const btnPlay = document.getElementById('play');
+const btnSelect = document.getElementById('level_user');
 
-for (let i = 1; i <= 100; i++) {
+
+btnPlay.addEventListener('click', function(){
+    if (btnSelect.value == 'easy'){
+        clickEasy();
+    } else if (btnSelect.value == 'medium'){
+        clickMedium();
+    } else if (btnSelect.value == 'hard'){
+        clickHard();
+    } else {
+        areaContainBoxes.innerHTML = 'Scegli la difficoltà'
+    }
+})
+
+function clickHard(){
+    areaContainBoxes.innerHTML = '';
+    for (let i = 1; i <= 100; i++) {
     areaContainBoxes.innerHTML += `<div class="box">${i}</div>`   
+    }
+    
 }
+
+function clickMedium(){
+    areaContainBoxes.innerHTML = '';
+    for (let i = 1; i <= 81; i++) {
+    areaContainBoxes.innerHTML += `<div class="box">${i}</div>`   
+    }
+}
+
+function clickEasy(){
+    areaContainBoxes.innerHTML = '';
+    for (let i = 1; i <= 49; i++) {
+    areaContainBoxes.innerHTML += `<div class="box">${i}</div>`   
+    }
+}
+
+
+for (let i = 0; i < checkBox.length ; i++) {
+    checkBox.addEventListener('click', function(){
+        checkBox[i].classList.add('select');
+    });
+}
+
